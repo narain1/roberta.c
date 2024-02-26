de5fb4a4d79f17d729e1c3809dce6124d2c9234d
narain1 <narainp@outlook.com>
2024-02-26 08:40:33 -0700

    update roberta model binary

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # roberta.c
+
+### TODO
+* mmap model load
+* separate model operations and tensor
--- roberta.c
+++ roberta.c
@@ -20,6 +20,73 @@ typedef struct {
   unsigned char byte_pieces[512];
 } Tokenizer;
 
+typedef struct {
+  int dim;
+  int hidden_dim;
+  int n_layers;
+  int n_heads;
+  int vocab_size;
+  int seq_len;
+} ModelConfig;
+
+ModelConfig base_config = {
+  .dim = 768,
+  .hidden_dim = 3072,
+  .n_layers = 12,
+  .n_heads = 8,
+  .vocab_size = 30522,
+  .seq_len = 512
+};
+
+struct Tensor {
+  int *data;
+  int *shape;
+};
+
+struct RobertaLayer {
+  struct Tensor *ln_attn_w;
+  struct Tensor *ln_attn_b;
+  
+  struct Tensor *ln_out_w;
+  struct Tensor *ln_out_b;
+
+  struct Tensor *q_w;
+  struct Tensor *q_b;
+  struct Tensor *k_w;
+  struct Tensor *k_b;
+  struct Tensor *v_w;
+  struct Tensor *v_b;
+
+  struct Tensor *o_w;
+  struct Tensor *o_b;
+  
+  struct Tensor *ff_i_w;
+  struct Tensor *ff_i_b;
+
+  struct Tensor *ff_o_w;
+  struct Tensor *ff_o_b;
+};
+
+struct EmbeddingLayer {
+  struct Tensor *Embedding_w;
+  struct Tensor *pos_w;
+  struct Tensor *token_type;
+  struct Tensor *ln_a;
+  struct Tensor *ln_b;
+};
+
+struct RobertaModel {
+  ModelConfig *config;
+  struct EmbeddingLayer *embed;
+  struct RobertaLayer *layers;
+};
+
+struct RobertaLayer load_model(struct RobertaModel *model, const char *fname) {
+  printf("%s: loading model mode from '%s'\n", __func__, fname);
+}
+  
+  
+
 void build_tokenizer(Tokenizer *t, char* tokenizer_path, int vocab_size) {
   t->vocab_size = vocab_size;
   t->vocab = (char**)malloc(vocab_size * sizeof(char*));
@@ -34,6 +101,7 @@ void build_tokenizer(Tokenizer *t, char* tokenizer_path, int vocab_size) {
   FILE *file = fopen(tokenizer_path, "rb");
   if (!file) { fprintf(stderr, "couldn't load %s\n", tokenizer_path); exit(EXIT_FAILURE); }
   if (fread(&t->max_token_length, sizeof(int), 1, file) != 1) {fprintf(stderr, "failed read\n"); exit(EXIT_FAILURE); }
+  printf("max token length %d\n", t->max_token_length);
   int len;
   for (int i=0; i<vocab_size; i++) {
     if (fread(t->vocab_scores + i, sizeof(float), 1, file) != 1) { fprintf(stderr, "failed read\n"); exit(EXIT_FAILURE); }
@@ -111,10 +179,27 @@ void bpe_encode(Tokenizer *t, char *text, int8_t bos, int8_t eos, int *tokens, i
   free(str_buffer);
 }
 
+char* decode(Tokenizer* t, int prev_token, int token) {
+    char *piece = t->vocab[token];
+    // following BOS (1) token, sentencepiece decoder strips any leading whitespace (see PR #89)
+    if (prev_token == 1 && piece[0] == ' ') { piece++; }
+    // careful, some tokens designate raw bytes, and look like e.g. '<0x01>'
+    // parse this and convert and return the actual byte
+    unsigned char byte_val;
+    if (sscanf(piece, "<0x%02hhX>", &byte_val) == 1) {
+        piece = (char*)t->byte_pieces + byte_val * 2;
+    }
+    return piece;
+}
+
+
 
 void main() {
   Tokenizer tokenizer;
-  uint32_t vocab_size = 50265;
+  uint32_t vocab_size = 30522;
+  uint32_t hidden_dim = 768;
+  uint32_t n_layers = 12;
+  uint32_t ff_dim = 3072;
   char *tokenizer_path = "tokenizer.bin";
   build_tokenizer(&tokenizer, tokenizer_path, vocab_size);
   printf("loaded tokenizer\n");
@@ -130,8 +215,16 @@ void main() {
   for (int i=0; i<num_prompt_tokens; i++) {
     printf("%d, ", prompt_tokens[i]);
   }
+  printf("\n");
+
+  int prev_token=prompt_tokens[0], next_token=prompt_tokens[1];
+  for (int i=0; i<num_prompt_tokens; i++) {
+    prev_token = i=0 ? prompt_tokens[0] : next_token;
+    char *token = decode(&tokenizer, prev_token, next_token);
+    printf("%s ", token);
+  }
+  printf("\n");
     
-  printf("token encoding done\n");
   free_tokenizer(&tokenizer);
   printf("freed tokenizer");
 }
